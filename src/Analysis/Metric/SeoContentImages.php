f079d92a993c47f94cd8ea98df9adcd9fe1fa5aa
stexaz <stexaz@gmail.com>
2013-10-16 18:14:18 +0300

    Look for images on page and display number of images found.

--- src/Analysis/Metric/SeoContentImages.php
+++ src/Analysis/Metric/SeoContentImages.php
@@ -11,11 +11,27 @@ class SeoContentImages extends Metric
     protected $solve_level      = 'easy';
     protected $pass_level       = 'pass';
 
+    public function getImagesCount()
+    {
+        $dom = $this->getAnalyzer()->getPage()->getSimpleHtmlDomObject();
+        $i = 0;
+        $n = 0;
+        while($dom->find('img',$i++)) $n++;
+        return $n;
+    }
     /**
      * @todo finish
      */
     public function process()
     {
-        $this->setOutput('No images found on this website.');
+        $images = $this->getImagesCount();
+        if (!$images) {
+            $this->setOutput('No images found on this website.');
+            $this->setPassLevel('fail');
+        } else {
+            $this->setOutput(sprintf('We found %d images on this website', $images));
+            $this->setPassLevel('pass');
+        }
+
     }
 }
