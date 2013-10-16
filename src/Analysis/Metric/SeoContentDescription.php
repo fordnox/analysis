92ac79aedf69ea9450e68b1c44df5109ade57920
stexaz <stexaz@gmail.com>
2013-10-16 17:39:46 +0300

    Find if site contains description and output it if found.

--- src/Analysis/Metric/SeoContentDescription.php
+++ src/Analysis/Metric/SeoContentDescription.php
@@ -11,12 +11,26 @@ class SeoContentDescription extends Metric
     protected $solve_level      = 'easy';
     protected $pass_level       = 'fail';
 
+    public function getDescription()
+    {
+        $dom = $this->getAnalyzer()->getPage()->getSimpleHtmlDomObject();
+        $description = $dom->find('meta[name=description]', 0);
+        return $description->content;
+    }
     /**
      * @todo finish
      */
     public function process()
     {
-        $this->setPassLevel('fail');
-        $this->setOutput('Missing');
+        $description = $this->getDescription();
+        if ($description) {
+            $this->setPassLevel('pass');
+            $this->setOutput(sprintf('<p>Great, there\'s description: %s</p>', $description));
+        } else {
+            $this->setPassLevel('fail');
+            $this->setOutput('<p>Description is missing</p>');
+        }
+
+
     }
 }
