e5220c0e9c544f7963019c70f9d09a31c3425aba
stexaz <stexaz@gmail.com>
2013-10-23 16:44:56 +0300

    Encoding detection implemented.

--- src/Analysis/Metric/TechnologiesEncoding.php
+++ src/Analysis/Metric/TechnologiesEncoding.php
@@ -11,11 +11,24 @@ class TechnologiesEncoding extends Metric
     protected $solve_level      = 'easy';
     protected $pass_level       = 'pass';
 
-    /**
-     * @todo finish
-     */
+    private function getEncoding()
+    {
+        $validator = $this->getAnalyzer()->getW3CValidator();
+        if ($validator) $validator = $validator->getSimpleHtmlDomObject();
+        $encoding = null;
+        if ($validator) $encoding = $validator->find('#charset',0);
+        if ($encoding) $encoding = $encoding->parent;
+        if ($encoding) $encoding = $encoding->parent;
+        if ($encoding) $encoding = $encoding->find('td', 0);
+        if ($encoding) $encoding = $encoding->innerText();
+        if (!$encoding) $encoding = 'Unknown';
+
+        return $encoding;
+    }
+
     public function process()
     {
-        $this->setOutput('UTF-8');
+        $encoding = $this->getEncoding();
+        $this->setOutput(strtoupper($encoding));
     }
 }
