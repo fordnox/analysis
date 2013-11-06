4c2218203ae3890a8f1931425abf966b7ce6bde8
stexaz <stexaz@gmail.com>
2013-11-06 18:41:20 +0200

    Implemented finding underscores in links.

--- src/Analysis/Metric/SeoBasicsUnderscoresInUrls.php
+++ src/Analysis/Metric/SeoBasicsUnderscoresInUrls.php
@@ -11,12 +11,31 @@ class SeoBasicsUnderscoresInUrls extends Metric
     protected $solve_level      = 'easy';
     protected $pass_level       = 'fail';
 
+    private function getHrefUnderscore()
+    {
+        $dom = $this->getAnalyzer()->getPage()->getSimpleHtmlDomObject();
+        $href = $dom->find('[href*=_]');
+        $result = array();
+        foreach ($href as $h)
+        {
+            if (substr($h->href, 0, 4) != 'http') $result[] = $h->href;
+        }
+        return $result;
+    }
+
     /**
      * @todo finish
      */
     public function process()
     {
-        $this->setPassLevel('pass');
-        $this->setOutput('No');
+        $hrefs = $this->getHrefUnderscore();
+        $total = count($hrefs);
+        if ($total) {
+            $this->setPassLevel('fail');
+            $this->setOutput('Yes');
+        } else {
+            $this->setPassLevel('pass');
+            $this->setOutput('No');
+        }
     }
 }
