ba2feec40f2e279a877aa98ecced3bdd46c8f5f7
stexaz <stexaz@gmail.com>
2013-11-06 16:39:51 +0200

    Implemented micro format detection.

--- src/Analysis/Metric/UsabilityMicroformats.php
+++ src/Analysis/Metric/UsabilityMicroformats.php
@@ -2,6 +2,7 @@
 namespace Analysis\Metric;
 use Analysis\Metric;
 use Analysis\Exception;
+use Mf2;
 
 class UsabilityMicroformats extends Metric
 {
@@ -11,12 +12,18 @@ class UsabilityMicroformats extends Metric
     protected $solve_level      = 'hard';
     protected $pass_level       = 'pass';
 
-    /**
-     * @todo finish
-     */
+    public function getMicroFormats()
+    {
+        $content = $this->getAnalyzer()->getPage()->getContent();
+        $mf = Mf2\parse($content);
+        return $mf;
+    }
+
     public function process()
     {
-        $this->setPassLevel('fail');
-        $this->setOutput('We found 0 type(s) of Microformat');
+        $microformats = $this->getMicroFormats();
+        $items = count($microformats['items']);
+        if (!$items) $this->setPassLevel('fail');
+        $this->setOutput(sprintf('We found %d type(s) of Microformat', $items));
     }
 }
