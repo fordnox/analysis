407bd81212b97154bccf99135b54b2c8294e52de
stexaz <stexaz@gmail.com>
2013-11-07 16:24:36 +0200

    Implemented check if current url contains url-rewrite.

--- src/Analysis/Metric/SeoBasicsUrlRewrite.php
+++ src/Analysis/Metric/SeoBasicsUrlRewrite.php
@@ -11,12 +11,27 @@ class SeoBasicsUrlRewrite extends Metric
     protected $solve_level      = 'hard';
     protected $pass_level       = 'fyi';
 
+    private function isUrlClean()
+    {
+        $url = $this->getAnalyzer()->getPage()->getUrl();
+        if (strpos('.php', $url) !== false || strpos('&', $url) !== false || strpos('=', $url)!==false ) return false;
+        return true;
+    }
+
     /**
-     * @todo finish
+     * @todo implement inner-pages urls check
      */
     public function process()
     {
+        $clean = $this->isUrlClean();
+        if ($clean) {
+            $this->setPassLevel('pass');
+            $output = 'Perfect, your URLs look clean.';
+        } else {
+            $this->setPassLevel('fail');
+            $output = 'Your url is not clean, it does not have url-rewrite.';
+        }
         $this->setPassLevel('pass');
-        $this->setOutput('Perfect, your URLs look clean.');
+        $this->setOutput($output);
     }
 }
