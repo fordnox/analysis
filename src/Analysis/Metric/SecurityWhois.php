fdd98b1e09d747f23372583ee6cd0af490e90ea5
stexaz <stexaz@gmail.com>
2013-11-07 16:27:16 +0200

    Started WHOIS implementation. Currently shows owner and registrar.

--- src/Analysis/Metric/SecurityWhois.php
+++ src/Analysis/Metric/SecurityWhois.php
@@ -16,6 +16,26 @@ class SecurityWhois extends Metric
      */
     public function process()
     {
-        $this->setOutput('Show whois details');
+        $whois = $this->getAnalyzer()->getWhois();
+        $details = '';
+        $contact_data = '';
+        foreach ($whois['contacts'] as $role=>$owners) {
+            foreach ($owners as $owner){
+                $contact_data .= '<ul>';
+                foreach ($owner as $name=>$data) {
+                    if ($data) $contact_data .= '<li>'. ucfirst($name).': '.$data .'</li>';
+                }
+                $contact_data .= '</ul>';
+            }
+        }
+        $registrar_data = '<ul>';
+        foreach ($whois['registrar'] as $name=>$data) {
+            if ($data) $registrar_data .= '<li>'. ucfirst($name).': '.$data .'</li>';
+        }
+        $registrar_data .= '</ul>';
+        if ($contact_data) $details .= '<div><div class="title">Contact Data</div>'.$contact_data.'</div>';
+        if ($registrar_data) $details .= '<div><div class="title">Registrar Data</div>'.$registrar_data.'</div>';
+        if (!$details) $details = 'No whois data found';
+        $this->setOutput($details);
     }
 }
