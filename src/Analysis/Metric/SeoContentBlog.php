5cbd0b94da5925c8a95f5ee6d2c143fbd0b661e0
stexaz <stexaz@gmail.com>
2013-11-07 13:19:39 +0200

    Implemented check if block.

--- src/Analysis/Metric/SeoContentBlog.php
+++ src/Analysis/Metric/SeoContentBlog.php
@@ -16,7 +16,66 @@ class SeoContentBlog extends Metric
      */
     public function process()
     {
-        $this->setPassLevel('pass');
-        $this->setOutput('We found a Blog on this website.');
+        $blog = $this->isBlog();
+        if ($blog){
+            $this->setPassLevel('pass');
+            $output = 'We found a Blog on this website.';
+        } else {
+            $this->setPassLevel('fail');
+            $output = 'We found no Blog on this website.';
+        }
+        $this->setOutput($output);
     }
+
+    private function isBlog()
+    {
+        $wp = $this->isWordPress();
+        $blogger = $this->isBlogger();
+        $pivotx = $this->isPivotX();
+        $atom = $this->hasAtom();
+        $rss = $this->hasRSS();
+        return $wp || $blogger || $pivotx || $atom || $rss;
+
+        }
+
+    private function isWordPress()
+    {
+        $dom = $this->getAnalyzer()->getPage()->getSimpleHtmlDomObject();
+        $wp = $dom->find('link[href*=wp-includes]', 0);
+        if ($wp) return true;
+        return false;
+    }
+
+    private function isBlogger()
+    {
+        $dom = $this->getAnalyzer()->getPage()->getSimpleHtmlDomObject();
+        $blogger = $dom->find('link[href*=blogger]', 0);
+        if ($blogger) return true;
+        return false;
+    }
+
+    private function isPivotX()
+    {
+        $dom = $this->getAnalyzer()->getPage()->getSimpleHtmlDomObject();
+        $pivot = $dom->find('div#pivotx-nav', 0);
+        if ($pivot) return true;
+        return false;
+    }
+
+    private function hasAtom()
+    {
+        $dom = $this->getAnalyzer()->getPage()->getSimpleHtmlDomObject();
+        $atom = $dom->find('link[href*=atom]', 0);
+        if ($atom) return true;
+        return false;
+    }
+
+    private function hasRSS()
+    {
+        $dom = $this->getAnalyzer()->getPage()->getSimpleHtmlDomObject();
+        $rss = $dom->find('link[href*=rss]', 0);
+        if ($rss) return true;
+        return false;
+    }
+
 }
