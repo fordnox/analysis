9e696087a7448eada4d32c3b865891427a03cda5
stexaz <stexaz@gmail.com>
2013-11-08 13:38:03 +0200

    Implemented web of trust.

--- src/Analysis/Metric/SecurityWot.php
+++ src/Analysis/Metric/SecurityWot.php
@@ -11,11 +11,41 @@ class SecurityWot extends Metric
     protected $solve_level      = 'easy';
     protected $pass_level       = 'pass';
 
-    /**
-     * @todo finish
-     */
+    private function getWot()
+    {
+        $api = 'http://api.mywot.com/0.4/public_link_json?hosts=';
+        $domain = $this->getAnalyzer()->getPage()->getDomainName();
+        $page = new \Analysis\Page();
+        $page->setUrl($api . $domain .'/');
+        $result = json_decode($page->getContent(), true);
+        return $result;
+    }
+
+    private function getReputation($wot)
+    {
+        $reputation = 'Very poor';
+        if ($wot >= 20) $reputation = 'Poor';
+        if ($wot >= 40) $reputation = 'Unsatisfactory';
+        if ($wot >= 60) $reputation = 'Good';
+        if ($wot >= 80) $reputation = 'Excellent';
+        return $reputation;
+    }
+
     public function process()
     {
-        $this->setOutput('Table with WOT details');
+        $wot = $this->getWot();
+        $wot = reset($wot);
+        $output = '<table>
+        <tr><th></th><th>Reputation</th><th>Confidence</th></tr>
+        <tr>
+            <td>Trustworthiness</td><td>%s</td><td>%d%%</td>
+        </tr>
+        <tr>
+            <td>Child safety</td><td>%s</td><td>%d%%</td>
+        </tr>
+        </table>';
+        $output = sprintf($output, $this->getReputation($wot[0][0]), $wot[0][1],
+                $this->getReputation($wot[4][0]), $wot[4][1]);
+        $this->setOutput($output);
     }
 }
