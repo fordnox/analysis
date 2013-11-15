e3692420d4a4e78018a3b166f9f303589bfb7dcc
stexaz <stexaz@gmail.com>
2013-11-15 18:33:53 +0200

    Implemented conversion form check.

--- src/Analysis/Metric/UsabilityConversionForms.php
+++ src/Analysis/Metric/UsabilityConversionForms.php
@@ -11,12 +11,39 @@ class UsabilityConversionForms extends Metric
     protected $solve_level      = 'easy';
     protected $pass_level       = 'pass';
 
-    /**
-     * @todo finish
-     */
     public function process()
     {
-        $this->setPassLevel('fail');
-        $this->setOutput('We could not find a Conversion Form on this page.');
+        $max_inputs = 10;
+        $input_count = $this->countInputs();
+        $password_fields = $this->countPasswordFields();
+        $captcha = $this->hasCaptcha();
+        $conversion = $input_count <= $max_inputs && $password_fields < 2 && !$captcha;
+        if ($conversion) {
+            $output = 'Perfect, we found a Conversion Form on this page.';
+            $this->setPassLevel('pass');
+        } else {
+            $output = 'We could not find a Conversion Form on this page.';
+            $this->setPassLevel('fail');
+        }
+
+        $this->setOutput($output);
+    }
+
+    private function countInputs()
+    {
+        return $this->getAnalyzer()->getPage()->countSelector('form input');
+    }
+
+    private function countPasswordFields()
+    {
+        return $this->getAnalyzer()->getPage()->countSelector('input[type=password]');
+    }
+
+    private function hasCaptcha()
+    {
+        $dom = $this->getAnalyzer()->getPage()->getSimpleHtmlDomObject();
+        $cap = $dom->find('input[name=recaptcha_response_field],input[name=captcha],input[name=code],input[name=secret_code],input[name=answer],input[name=Turing],input[name=nucaptcha-answer],iframe[src*=recaptcha]', 0);
+        if ($cap) return true;
+        return false;
     }
 }
