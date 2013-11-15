e7e262ee58b1290cc92a340e00a54050571f630e
stexaz <stexaz@gmail.com>
2013-11-15 13:23:03 +0200

    Implemented checking some headers and script/style files count.

--- src/Analysis/Metric/TechnologiesSpeed.php
+++ src/Analysis/Metric/TechnologiesSpeed.php
@@ -11,21 +11,88 @@ class TechnologiesSpeed extends Metric
     protected $solve_level      = 'easy';
     protected $pass_level       = 'pass';
 
-    /**
-     * @todo finish
-     */
     public function process()
     {
-        $output='
-        <p>Congratulations! Your websites speed is fully optimized.</p>
-        <ul class="unstyled">
-            <li><i class="icon-ok"></i> Perfect, your server is using a caching method to speed up page display. Perfect, your website does not use nested tables.</li>
-            <li><i class="icon-ok"></i> Perfect, your website does not use inline styles.</li>
-            <li><i class="icon-ok"></i> Perfect, your website has few CSS files.</li>
-            <li><i class="icon-ok"></i> Perfect, your website has few JavaScript files.</li>
-            <li><i class="icon-ok"></i> Perfect, your website takes advantage of gzip.</li>
-        </ul>
-        ';
+        $optimal_css = 3;
+        $optimal_js = 5;
+
+        $cache = $this->hasCache();
+        $inline = $this->hasInlineCss();
+        $css = $this->countCssFiles();
+        $js = $this->countJsFiles();
+        $gz = $this->hasGzip();
+
+        $output='';
+        if ($cache && $inline && $css <= $optimal_css && $js <= $optimal_js && $gz)
+            $output.='<p>Congratulations! Your websites speed is fully optimized.</p>';
+        else
+            $output.='<p>You should better optimise your website.</p>';
+        $output.='<ul class="unstyled">';
+        $output.=sprintf('<li><i class="icon-%s"></i> %s</li>',
+            $cache?'ok':'minus',
+            $cache?'Perfect, your server is using a caching method to speed up page display. Perfect, your website does not use nested tables.':'You should use cache to fasten your page loading.'
+        );
+        $output.=sprintf('<li><i class="icon-%s"></i> %s</li>',
+            $inline?'minus':'ok',
+            $inline?'Too bad, your site uses inline css styles. They should be put on a separate file to fasten the page loading.':'Perfect, your website does not use inline styles.'
+        );
+        $output.=sprintf('<li><i class="icon-%s"></i> %s</li>',
+            $css <= $optimal_css?'ok':'minus',
+            $css <= $optimal_css?'Perfect, your website has few CSS files.':'Too bad, your site has lots of CSS files.'
+        );
+        $output.=sprintf('<li><i class="icon-%s"></i> %s</li>',
+            $js <= $optimal_js?'ok':'minus',
+            $js <= $optimal_js?'Perfect, your website has few JavaScript files.':'Too bad, your site has lots of JavaScript files.'
+        );
+        $output.=sprintf('<li><i class="icon-%s"></i> %s</li>',
+            $gz?'ok':'minus',
+            $gz?'Perfect, your website takes advantage of gzip.':'Too bad, your site does not use gzip compression.'
+        );
+
         $this->setOutput($output);
+    }
+
+    private function hasCache()
+    {
+        $cc = $this->getPage()->getHeader('Cache-Control');
+        if (!$cc) return false;
+        if (stripos($cc, 'no-cache') !== false) return false;
+        if (stripos($cc, 'public') !== false) return true;
+        if (stripos($cc, 'private') !== false) return true;
+        if (stripos($cc, 'max-age') !== false) return true;
+        return false;
+    }
+
+    private function hasInlineCss()
+    {
+        $dom = $this->getAnalyzer()->getPage()->getSimpleHtmlDomObject();
+        if ($dom->find('[style]', 0)) return true;
+        return false;
+    }
+
+    private function countCssFiles()
+    {
+        $dom = $this->getAnalyzer()->getPage()->getSimpleHtmlDomObject();
+        $n = 0;
+        while($im=$dom->find('style[src], link[rel=stylesheet]',$n)){
+            $n++;
+        }
+        return $n;
+    }
+
+    private function countJsFiles()
+    {
+        $dom = $this->getAnalyzer()->getPage()->getSimpleHtmlDomObject();
+        $n = 0;
+        while($im=$dom->find('script[src]',$n)){
+            $n++;
+        }
+        return $n;
+    }
+
+    private function hasGzip()
+    {
+        $ae = $this->getPage()->getHeader('Content-Encoding');
+        return stripos($ae, 'gzip') !== false;
     }
 }
