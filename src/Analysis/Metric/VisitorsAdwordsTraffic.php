d0d1e9cdab09699801c28c7a0ca000ff0fe42e0c
stexaz <stexaz@gmail.com>
2013-11-20 18:14:10 +0200

    Implemented Adwords traffic chart.

--- src/Analysis/Metric/VisitorsAdwordsTraffic.php
+++ src/Analysis/Metric/VisitorsAdwordsTraffic.php
@@ -2,6 +2,8 @@
 namespace Analysis\Metric;
 use Analysis\Metric;
 use Analysis\Exception;
+use Analysis\Page;
+use Analysis\SEMRushApi;
 
 class VisitorsAdwordsTraffic extends Metric
 {
@@ -11,15 +13,35 @@ class VisitorsAdwordsTraffic extends Metric
     protected $solve_level      = 'easy';
     protected $pass_level       = 'fyi';
 
-    /**
-     * @todo finish
-     */
+    public function getTraffic()
+    {
+        $domain = $this->getAnalyzer()->getPage()->getDomainName();
+        $sm = new SEMRushApi();
+        $sm->setDomain($domain);
+        return $sm->AdwordsTraffic();
+    }
+
+    private function toPercent($data)
+    {
+        $sum = array_sum($data);
+        $result = array();
+        foreach ($data as $k=>$v) {
+            if ($sum == 0) {
+                $result[$k] = 0;
+            } else {
+                $result[$k] = round($v/$sum*100);
+            }
+        }
+        return $result;
+    }
+
     public function process()
     {
-        $adwords = 10;
-        $organic = 90;
+        $traffic = $this->getTraffic();
 
-        $output = '<img src="https://chart.googleapis.com/chart?cht=p&chd=s:10,90&chs=200x100&chdl=AdWords|Organic" alt="Pie">';
+        $traffic = $this->toPercent($traffic[0]);
+
+        $output = sprintf('<img src="https://chart.googleapis.com/chart?cht=p&chd=t:%d,%d&chs=200x100&chdl=AdWords|Organic" alt="Traffic">', $traffic['At'], $traffic['Ot']);
         $this->setOutput($output);
     }
 }
